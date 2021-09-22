636a7ab329f157a27dfee3b284928e4ca93b9fd7
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-22 21:12:43 +0530

    Use case2 regarding addition of null mood is added

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -2,13 +2,34 @@ package com.moodanalyser;
 
 public class MoodAnalyser {
 
-	public String analyseMood(String message) {
-		if (message.contains("Sad"))
-			return "SAD";
-		else
-			return "HAPPY";
-		// TODO Auto-generated method stub
 
+	private String message;
+
+	public MoodAnalyser() {
+		this.message = null;
 	}
 
+	/*
+	 * The method to take in the message via a constructor
+	 */
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	/*
+	 * The method to read the message and return Happy or sad
+	 */
+	public String analyseMood() {
+		try {
+			if (message.contains("Sad"))
+				return "SAD";
+			else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
+			return "HAPPY";
+			// TODO Auto-generated method stub
+
+		}
+	}
 }

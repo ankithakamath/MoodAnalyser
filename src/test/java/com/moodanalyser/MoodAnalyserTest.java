5e0f86ae1d6d66a82a843c74de1be05a367ec1ee
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-22 20:55:47 +0530

    Use case 1 refactored using a Constructor is added

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -9,14 +9,15 @@ public class MoodAnalyserTest {
 
 	@Test
 	public void GivenMessage_ReturnSad_WhenSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-	String mood = moodAnalyser.analyseMood("I am in Sad mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad mood ");
+	String mood = moodAnalyser.analyseMood();
 	Assert.assertEquals("SAD",mood);
 	}
 
 	@Test
 	public void GivenMessage_ReturnHappy_WhenNotSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-	String mood = moodAnalyser.analyseMood("I am in Happy mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy mood");
+	String mood = moodAnalyser.analyseMood();
+	Assert.assertEquals("HAPPY",mood);
  }
 }
